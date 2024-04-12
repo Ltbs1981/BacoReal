5ac08d174103c6549ab64aff00eeb4daa2bf43b0
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-04-12 00:18:16 -0300

    Projeto pronto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using BancoReal;
+
+Usuario joao = new Usuario("João", 1000);
+joao.Sacar(300);
+joao.Depositar(500);
+joao.Imprimir();
+Usuario maria = new Usuario("Maria", 2000);
+maria.Depositar(700);
+maria.Sacar(300);
+joao.Depositar(300);
+
+
+maria.Imprimir();
+joao.Imprimir();
---
+++ Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoReal
+{
+    class Usuario
+    {
+        public string Titular;
+        public double Saldo;
+
+        public Usuario(string titular, double saldo)
+        {
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"O valor de R$ {valor}, foi cretitado em sua conta");
+        }
+        public void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+            else
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Saque de {valor} realizado com sucesso. Novo saldo: {Saldo}");
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine($"Saldo atual da conta de {Titular}: {Saldo}");
+        }
+
+    }
+}
+    
